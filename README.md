1004ff966b44b677753e930d26778ca12fe22117
Callum Cameron <cjcameron7@gmail.com>
2014-06-12 10:44:53 +0100

    Time parsing.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-argtypes
+Argtypes
 ========
 
-Argument validation helpers for the Python argparse module
+The `type` argument of Argparse's `add_argument` method lets the parser do validation and type conversion of arguments. This module provides a few convenient helper functions for use with `type`.
+
+    import argparse
+    import argtypes
+
+    parser = argparse.ArgumentParser(description="Argtypes example")
+    parser.add_argument("num_runs", type=argtypes.positive_int)
+    parser.add_argument("dir", type=argtypes.existing_dir)
+    args = parser.parse_args()
+    print args
+
+Requirements
+------------
+
+`pytimeparse` (which requires `future`):
+
+    pip install pytimeparse future
+
+Setup
+-----
+
+1. Clone this repo.
+2. Add it to your PYTHONPATH.
--- argtypes.py
+++ argtypes.py
@@ -1,74 +1,97 @@
 """Helper functions for the 'type' argument of argparse's add_argument method."""
 
 import argparse
+import pytimeparse.timeparse
 import os.path
 
-def positive_int(arg):
-    """Test whether arg is a positive integer, and convert it to int"""
-    i = int(arg)
-    if i <= 0:
-        raise argparse.ArgumentTypeError("'%d' is not a positive integer" % i)
-    return i
+class IntCmp(object):
+    """Check that arg is an integer satisfying a condition"""
+    def __init__(self, val, description):
+        super(IntCmp, self).__init__()
+        self.val = int(val)
+        self.description = description
 
-class IntGreater(object):
-    def __init__(self, val):
-        super(IntGreater, self).__init__()
-        self.val = val
-
-    def __call__(self, arg):
-        i = int(arg)
-        if i <= self.val:
-            raise argparse.ArgumentTypeError("'%d' is not greater than '%d'" % (i, self.val))
-        return i
-
-class IntLess(object):
-    def __init__(self, val):
-        super(IntLess, self).__init__()
-        self.val = val
+    def valid(self, arg):
+        """Boolean: does the arg satisfy the condition?"""
+        raise NotImplementedError
 
     def __call__(self, arg):
-        i = int(arg)
-        if i >= self.val:
-            raise argparse.ArgumentTypeError("'%d' is not less than '%d'" % (i, self.val))
-        return i
+        try:
+            i = int(arg)
+            if not self.valid(i):
+                raise argparse.ArgumentTypeError("'%d' is not %s %d" % (i, self.description, self.val))
+            return i
+        except ValueError:
+            raise argparse.ArgumentTypeError("'%s' is not an integer %s %d" % (arg, self.description, self.val))
 
-class IntGreaterEq(object):
+
+class IntGreater(IntCmp):
+    """Check that arg is greater than val"""
     def __init__(self, val):
-        super(IntGreaterEq, self).__init__()
-        self.val = val
+        super(IntGreater, self).__init__(val, "greater than")
 
-    def __call__(self, arg):
-        i = int(arg)
-        if i < self.val:
-            raise argparse.ArgumentTypeError("'%d' is not greater than or equal to '%d'" % (i, self.val))
-        return i
+    def valid(self, arg):
+        return arg > self.val
 
-class IntLessEq(object):
+class IntLess(IntCmp):
+    """Check that arg is less than val"""
     def __init__(self, val):
-        super(IntLessEq, self).__init__()
-        self.val = val
+        super(IntLess, self).__init__(val, "less than")
 
-    def __call__(self, arg):
-        i = int(arg)
-        if i > self.val:
-            raise argparse.ArgumentTypeError("'%d' is not less than or equal to '%d'" % (i, self.val))
-        return i
+    def valid(self, arg):
+        return arg < self.val
+
+class IntGreaterEq(IntCmp):
+    """Check that arg is greater than or equal to val"""
+    def __init__(self, val):
+        super(IntGreaterEq, self).__init__(val, "greater than or equal to")
+
+    def valid(self, arg):
+        return arg >= self.val
+
+class IntLessEq(IntCmp):
+    """Check that arg is less than or equal to val"""
+    def __init__(self, val):
+        super(IntLessEq, self).__init__(val, "less than or equal to")
+
+    def valid(self, arg):
+        return arg <= self.val
+
+positive_int = IntGreater(0)
+
 
 def absolute_path(arg):
+    """Convert arg to an absolute path"""
     return os.path.realpath(arg)
 
 def existing_path(arg):
+    """Make sure arg exists"""
     if not os.path.exists(arg):
         raise argparse.ArgumentTypeError("path '%s' does not exist" % arg)
     return arg
 
 def exitsing_absolute_path(arg):
-    return os.path.realpath(existing_path(arg))
+    """Make sure arg exists, and convert it to an absolute path"""
+    return absolute_path(existing_path(arg))
 
 def existing_dir(arg):
+    """Make sure arg is a directory"""
     if not os.path.isdir(arg):
         raise argparse.ArgumentTypeError("directory '%s' does not exist" % arg)
     return arg
 
 def exitsing_absolute_dir(arg):
-    return os.path.realpath(existing_dir(arg))
+    """Make sure arg is a directory, and convert it to an absolute path"""
+    return absolute_path(existing_dir(arg))
+
+def time_in_seconds(arg):
+    """Parse arg as a time in seconds. See pytimeparse for the formats this
+    accepts. Only accepts whole numbers of seconds."""
+    try:
+        return int(arg)
+    except ValueError:
+        i = pytimeparse.timeparse.timeparse(arg)
+        if i is None or isinstance(i, float):
+            raise argparse.ArgumentTypeError("'%s' is not a valid time" % arg)
+        else:
+            return i
